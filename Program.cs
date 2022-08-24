961f27d43a2006da8e682e40f549c225ed02ac7b
Coi <thithibuibui1@gmail.com>
2022-08-24 18:03:11 +0700

    da sua lai

--- Program.cs
+++ Program.cs
@@ -7,7 +7,17 @@ using System.Threading.Tasks;
 namespace ConsoleApp1
 {
     class Program
-    {
+    {   
+        static void input_arr(int [] arr ,int n)
+        {
+            
+            for(int i = 0; i < n; i++)
+            {
+                string nhap = Console.ReadLine();
+                int temp = Convert.ToInt32(nhap);
+                arr[i] = temp;
+            } 
+        }
         static void min_max()
         {
             Console.Write("Nhap n phan tu: ");
@@ -31,13 +41,13 @@ namespace ConsoleApp1
             Console.ReadLine();
         }
 
-        static int So_Phan_Tu_Khac_Nhau(int[] arr, ref int[] arr1, int n)
+        static int So_Phan_Tu_Khac_Nhau(int[] arr, ref int[] arr1)
         {
-            arr1 = new int[n];
+            arr1 = new int[arr.Length];
             int x = 1;
             arr1[0] = arr[0];
 
-            for (int i = 1; i < n; i++)
+            for (int i = 1; i < arr.Length; i++)
             {
                 int count = 0;
                 for (int j = 0; j < x; j++)
@@ -53,35 +63,30 @@ namespace ConsoleApp1
             }
             return x;
         }
-        static void SL_Xuat_hien()
-        {
-            Console.Write("Nhap n phan tu: ");
-            string n = Console.ReadLine();
-            int a = Convert.ToInt32(n);
-            int[] arr = new int[a];
+        static void SL_Xuat_hien(int [] arr, int a)
+        {  
             int[] save = new int[a];
-            for (int i = 0; i < a; i++)
-            {
-                string nhap = Console.ReadLine();
-                int temp = Convert.ToInt32(nhap);
-                arr[i] = temp;
-            }
-            So_Phan_Tu_Khac_Nhau(arr, ref save, a);
-            for (int i = 0; i < So_Phan_Tu_Khac_Nhau(arr, ref save, a); i++)
+            So_Phan_Tu_Khac_Nhau(arr, ref save);
+            for (int i = 0; i < So_Phan_Tu_Khac_Nhau(arr, ref save); i++)
             {
                 int temp = save[i];
                 int count = 0;
-                for (int j = 0; j < a; j++)
+                for (int j = 0; j < arr.Length; j++)
                 {
                     if (temp == arr[j]) count++;
                 }
-                Console.WriteLine(temp + ": " + count + "TIme");
+                Console.WriteLine(temp + ": " + count + " TIme");
             }
             Console.ReadKey();
         }
         static void Main(string[] args)
         {
-            SL_Xuat_hien();
+
+            Console.WriteLine("Nhap so luong phan tu n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int [] arr = new int[n];    
+            input_arr(arr, n);
+            SL_Xuat_hien(arr, n);
         }
     }
 }
